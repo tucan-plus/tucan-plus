72e4e3a1a7bd4e703ae96bb2509df28f2e0d7bba
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-10-12 19:33:07 +0200

    Small fix

--- crates/tucan-plus-worker/src/lib.rs
+++ crates/tucan-plus-worker/src/lib.rs
@@ -201,20 +201,23 @@ fn prep_planning(
 }
 
 impl RequestResponse for RecursiveAnmeldungenRequest {
-    type Response = RecursiveAnmeldungenResponse;
+    type Response = Option<RecursiveAnmeldungenResponse>;
 
     fn execute(&self, connection: &mut SqliteConnection) -> Self::Response {
         let root = AnmeldungenRootRequest {
             course_of_study: self.course_of_study.clone(),
         }
         .execute(connection);
-        assert_eq!(root.len(), 1);
-        prep_planning(
+        assert!(root.len() <= 1);
+        if root.is_empty() {
+            return None;
+        }
+        Some(prep_planning(
             connection,
             &self.course_of_study,
             &self.expanded,
             root[0].clone(),
-        )
+        ))
     }
 }
 
