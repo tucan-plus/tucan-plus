aa1c9565cbc375c1103c11bdaf9896835049b63f
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-02-14 18:24:55 +0100

    chrome fix

--- tucan-plus-extension/content-script-redirect.js
+++ tucan-plus-extension/content-script-redirect.js
@@ -3,7 +3,10 @@ url = url.replaceAll(new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/script
     "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N$1,-N000019,");
 url = url.replaceAll(new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/scripts/mgrqispi\\.dll\\?APPNAME=CampusNet&PRGNAME=CHANGELANGUAGE&ARGUMENTS=-N(\\d+),-N002$", "g"),
     "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N$1,-N000350,");
+url = url.replaceAll(new RegExp("^https://www\\.tucan\\.tu-darmstadt\\.de/scripts/mgrqispi\\.dll\\?APPNAME=CampusNet&PRGNAME=STARTPAGE_DISPATCH&ARGUMENTS=-N(\\d+),-N(\\d+),-N000000000000000$", "g"),
+    "https://www.tucan.tu-darmstadt.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N$1,-N$2,");
 if (url !== window.location.href) {
+    console.log("REDIRECTING")
     window.location.href = url;
 } else {
     console.log("NOT REDIRECTING")
