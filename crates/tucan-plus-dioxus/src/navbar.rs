a86c5ba715ff8dab6542859d47feb5962650c70b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-02-14 22:13:50 +0100

    wip

--- crates/tucan-plus-dioxus/src/navbar.rs
+++ crates/tucan-plus-dioxus/src/navbar.rs
@@ -2,9 +2,8 @@ use dioxus::prelude::*;
 use tucan_types::{LoginResponse, RevalidationStrategy, Tucan};
 
 use crate::{
-    LOGO_SVG, RcTucanType, Route, common::handle_error, login_component::LoginComponent,
-    logout_component::LogoutComponent, navbar_logged_in::NavbarLoggedIn,
-    navbar_logged_out::NavbarLoggedOut,
+    LOGO_SVG, RcTucanType, Route, common::handle_error, logout_component::LogoutComponent,
+    navbar_logged_in::NavbarLoggedIn, navbar_logged_out::NavbarLoggedOut,
 };
 
 #[component]
@@ -110,7 +109,12 @@ pub fn Navbar() -> Element {
                         LogoutComponent {
                         }
                     } else {
-                        LoginComponent {
+                        a {
+                            id: "login-button",
+                            class: "btn btn-primary",
+                            role: "button",
+                            href: "https://dsf.tucan.tu-darmstadt.de/IdentityServer/External/Challenge?provider=dfnshib&returnUrl=%2FIdentityServer%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3DClassicWeb%26scope%3Dopenid%2520DSF%2520email%26response_mode%3Dquery%26response_type%3Dcode%26ui_locales%3Dde%26redirect_uri%3Dhttps%253A%252F%252Fwww.tucan.tu-darmstadt.de%252Fscripts%252Fmgrqispi.dll%253FAPPNAME%253DCampusNet%2526PRGNAME%253DLOGINCHECK%2526ARGUMENTS%253D-N000000000000001,ids_mode%2526ids_mode%253DY",
+                            "Login"
                         }
                     }
                 }
