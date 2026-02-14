ecd25717f27ed431f6006e8303bfbcbf59ca3cff
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-02-14 20:11:44 +0100

    tucan autologin

--- tucan-plus-extension/content-script-login.js
+++ tucan-plus-extension/content-script-login.js
@@ -1,9 +1,27 @@
 console.log("tucan-plus login")
 
-let passwordInput = /** @type { HTMLInputElement | null} */ (document.querySelector('#password'))
+const usernameInput = /** @type { HTMLInputElement | null} */ (document.querySelector('#username'))
+const passwordInput = /** @type { HTMLInputElement | null} */ (document.querySelector('#password'))
 
-if (passwordInput) {
+/**
+ * @param {HTMLInputElement} usernameInput
+ * @param {HTMLInputElement} passwordInput
+ */
+function handle(usernameInput, passwordInput) {
+    if (usernameInput.value !== "" && passwordInput.value !== "") {
+        /** @type { HTMLButtonElement } */ (document.querySelector('[name="_eventId_proceed"]')).click()
+    }
+}
+
+if (usernameInput && passwordInput) {
     passwordInput.addEventListener("change", (event) => {
-        console.log("ONCHANGE", passwordInput?.value)
+        handle(usernameInput, passwordInput)
     })
+    usernameInput.addEventListener("change", (event) => {
+        handle(usernameInput, passwordInput)
+    })
+}
+
+if (document.querySelector("#fudis_selected_token_ids_input")) {
+    /** @type { HTMLButtonElement } */ (document.querySelector('[name="_eventId_proceed"]')).click()
 }
